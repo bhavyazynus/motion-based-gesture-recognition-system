8e049df21e47a061639176d20056b8d485b29361
k4droid3 <k4droid3@gmail.com>
2020-09-14 12:15:56 +0530

    branch created with gesture.py changed to capture.py

---
+++ capture.py
@@ -0,0 +1,12 @@
+import urllib.request
+import cv2
+import numpy as np
+import timeURL = "http://192.168.43.1:8080"
+
+while True:    
+    img_arr = np.array(bytearray(urllib.request.urlopen(URL).read()),dtype=np.uint8)
+    img = cv2.imdecode(img_arr,-1)
+    cv2.imshow('IPWebcam',img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
