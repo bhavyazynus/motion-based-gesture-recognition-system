0f5e01381b26dfd2e4c0e56c73dd67bfb579d9ca
k4droid3 <k4droid3@gmail.com>
2020-09-14 12:54:04 +0530

    made capture.py work with IP webcams

--- capture.py
+++ capture.py
@@ -1,12 +1,12 @@
-import urllib.request
-import cv2
-import numpy as np
-import timeURL = "http://192.168.43.1:8080"
+import cv2 as c
 
-while True:    
-    img_arr = np.array(bytearray(urllib.request.urlopen(URL).read()),dtype=np.uint8)
-    img = cv2.imdecode(img_arr,-1)
-    cv2.imshow('IPWebcam',img)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+vid = c.VideoCapture('http://192.168.1.107:8080/video')
+
+while True:
+    ret,frame = vid.read()
+    c.imshow('Webcam',frame)
+    if c.waitKey(1) & 0xFF == ord('q'):
         break
+
+vid.release()
+c.destroyAllWindows()
