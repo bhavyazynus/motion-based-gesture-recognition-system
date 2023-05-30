1fb2b9b76524d0c3e23ee313b32a1a1d8dfa24c0
Bhavya Manoj Votavat <81924615+bhavyazynus@users.noreply.github.com>
2023-05-30 23:41:54 +0530

    Update img_test.py

--- img_test.py
+++ img_test.py
@@ -6,12 +6,15 @@ from keras.models import load_model
 from keras.models import model_from_json
 import json
 
+
 with open('model.json','r') as f:
     model_json = json.load(f)
 loaded_model = model_from_json(json.dumps(model_json))
 loaded_model.load_weights('epoch5_model.h5')
 
+
 images = ('A_test.jpg', 'nutu_test.jpeg')
+
 
 for i in images:
 	img = cv2.imread(i)
